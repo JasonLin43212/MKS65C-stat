cf62202ece7861d0d4ed4334af24a597eedccfe7
unknown <batmanthelegendly@gmail.com>
2018-10-30 21:32:07 -0400

    byte me

--- stats.c
+++ stats.c
@@ -9,11 +9,21 @@ int main() {
 
   stat("testfile.txt",&test);
 
-  printf("Showing stats of <testfile.txt>");
-  printf("Size: %d",test.st_size);
-  printf("Permissions: %ld",test.st_mode);
-  printf("Access time: %ld",test.st_atime);
-  
+  printf("Showing stats of <testfile.txt> \n");
+  printf("Size: %d \n",test.st_size);
+  char buffboi[50];
+  int byte = test.st_size;
+  int kb = byte / 1000;
+  int mb = kb / 1000;
+  int gb = mb / 1000;
+  sprintf(buffboi,"In bytes: %d \nIn KB: %d \nIn MB: %d \nIn GB: %d",byte,kb,mb,gb);
+  printf("%s\n",buffboi);
+
+
+
+  printf("Permissions: %ud \n",test.st_mode);
+  printf("Access time: %ld \n",test.st_atime);
+
 
   return 0;
 }
